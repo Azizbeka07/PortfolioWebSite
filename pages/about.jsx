a88a7e1fabba21abd8c37d1ba1fa7e5a7e65d0eb
Azizbek <you@example.com>
2023-07-16 21:44:51 +0500

    added header

---
+++ pages/about.jsx
@@ -0,0 +1,12 @@
+import Link from 'next/link';
+import React from 'react'
+
+const About = () => {
+  return (
+    <div>
+        <Link href='/portfolio' > Portfolio page link </Link>
+    </div>
+  )
+}
+
+export default About;
